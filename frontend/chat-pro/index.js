df191322ae13bc7a18e742e650ec7da6fb68ac59
Brayan Gamboa <bsgv2005mail.com>
2022-05-11 01:54:40 -0500

    Chat - 11/05/2022

--- frontend/chat-pro/index.js
+++ frontend/chat-pro/index.js
@@ -1,42 +1,44 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const path = require("path");
 const app = express();
 
 //Libraries WebSocket
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
-require('dotenv').config();
+require("dotenv").config();
 
 //Middewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname,'static')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "static")));
 
-app.get('', (req,res) => {
-    res.sendFile(path.join(__dirname,"static/html/registration.html"))
-    
-})
+app.get("", (req, res) => {
+  res.sendFile(path.join(__dirname, "static/html/registration.html"));
+});
 
 //variables
-app.set('PORT', process.env.PORT || 4050);
+app.set("PORT", process.env.PORT || 4050);
 
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
+io.on("connection", (socket) => {
+  console.log('Client connected...',socket.id);
+  socket.on("sendMessage", (data) => {
+    console.log(data);
+    io.emit("sendMessage", data);
+  });
 
-
-
-io.on('connection', (socket) => {
-    console.log('Client connected...',socket.id);
+  socket.on("disconnect", () => {
+    console.log('Client disconnected...',socket.id);
+  });
 });
 
-httpServer.listen(app.get('PORT'), () => {
-    console.log(`Server on port ${app.get('PORT')}`);
-});    
-
-
+httpServer.listen(app.get("PORT"), () => {
+  console.log(`Server on port ${app.get("PORT")}`);
+});
--- frontend/chat-pro/static/scripts/chat.js
+++ frontend/chat-pro/static/scripts/chat.js
@@ -1,3 +1,35 @@
-const { io } = require("socket.io");
+import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
+const socket = io("http://localhost:5020");
 
-const socket = io();
+let room = document.getElementById("room");
+let btn = document.getElementById("enviarDatos");
+let tablero = document.getElementById("tablero");
+let box = document.getElementById("box");
+let roomcontent = document.getElementById("room");
+let form = document.getElementById("form");
+
+const printMessage = (emailUser, message) => {
+  tablero.innerHTML += `<b>${emailUser}</b>: ${message} <br>`;
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const emailUser = JSON.parse(localStorage.getItem("user")).email;
+  const mensaje = document.getElementById("mensaje").value;
+  if (mensaje == '') {
+    swal("Error", "No puede enviar un mensaje vacio", "error");
+  } else {
+    socket.emit("sendMessage", { emailUser, mensaje });
+  }
+});
+
+socket.on('connect', function() {
+    let hola = 6
+    room = `Sala ${hola}`;
+    socket.emit('room', room);
+    roomcontent.innerHTML = room;
+});
+
+socket.on("sendMessage", (data) => {
+  printMessage(data.emailUser, data.mensaje);
+});
